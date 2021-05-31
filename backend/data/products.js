de330aade9ff6749c0ca5392cb3e5f14350ca255
kgauzampe <kgauza.mpe@gmail.com>
2021-05-31 17:36:42 +0200

    database connected

---
+++ backend/data/products.js
@@ -0,0 +1,9 @@
+const products = [
+    {
+        name: "Playstation 5",
+        imageUrl: "https://media.loot.co.za/static/gallery/previews/w/s/t/wstw-7053-g060/preview.wstw-7053-g060.1.jpg",
+        description: "The PS5™ Digital Edition unleashes new gaming possibilities that you never anticipated.",
+        price: 11000,
+        countInStock: 14
+    },
+]
